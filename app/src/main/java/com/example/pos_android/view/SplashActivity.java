8de855b7e6575e5c51b67a9c4f566ce80ccd9401
Aswin <aswinap99@gmail.com>
2022-10-13 21:10:49 +0530

    feature/login_ui

--- app/src/main/java/com/example/pos_android/view/SplashActivity.java
+++ app/src/main/java/com/example/pos_android/view/SplashActivity.java
@@ -1,18 +1,30 @@
 package com.example.pos_android.view;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-
 import com.example.pos_android.R;
+import com.example.pos_android.view.login.LoginActivity;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+
+            Intent i = new Intent(SplashActivity.this,
+                    LoginActivity.class);
+            startActivity(i);
+            finishAffinity();
+
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
