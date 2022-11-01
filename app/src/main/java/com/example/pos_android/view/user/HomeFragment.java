5e7d9860be0b66a6818a764e0c607937fc4910e6
Aswin <aswinap99@gmail.com>
2022-11-01 23:55:02 +0530

    reservation_ui_started

--- app/src/main/java/com/example/pos_android/view/user/HomeFragment.java
+++ app/src/main/java/com/example/pos_android/view/user/HomeFragment.java
@@ -1,14 +1,16 @@
 package com.example.pos_android.view.user;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
@@ -20,66 +22,29 @@ import com.example.pos_android.data.model.PopularModel;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     View view;
+    CardView bookingImage;
     List<SlideModel> slideModels = new ArrayList<>();
     ArrayList<PopularModel> popularArrayList = new ArrayList<>();
     ArrayList<PopularModel> recentArray = new ArrayList<>();
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
         ImageSlider imgSlider = view.findViewById(R.id.image_slider);
         RecyclerView popularRecyclerView = view.findViewById(R.id.popular_recyclerview);
         RecyclerView recentRecyclerview = view.findViewById(R.id.recent_recyclerview);
+        bookingImage = view.findViewById(R.id.iv_booking);
 
         initData();
 
@@ -90,7 +55,7 @@ public class HomeFragment extends Fragment {
         popularRecyclerView.setAdapter(popularAdapter);
         popularRecyclerView.setLayoutManager(new LinearLayoutManager(requireActivity(), RecyclerView.HORIZONTAL, false));
 
-         popularAdapter = new PopularAdapter(recentArray);
+        popularAdapter = new PopularAdapter(recentArray);
         recentRecyclerview.setAdapter(popularAdapter);
         recentRecyclerview.setLayoutManager(new LinearLayoutManager(requireActivity(), RecyclerView.HORIZONTAL, false));
 
@@ -112,5 +77,9 @@ public class HomeFragment extends Fragment {
         recentArray.add(new PopularModel("Cake", R.drawable.dm3, "Fish curry"));
         recentArray.add(new PopularModel("Brosted", R.drawable.dm1, "biriyaniii"));
         recentArray.add(new PopularModel("Mandhi", R.drawable.dm2, "delicious food"));
+
+        bookingImage.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_tableReservationFragment);
+        });
     }
 }
